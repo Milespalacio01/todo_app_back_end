2eb5fbe23c2691b5a959c1abbd6e0068b04fcf86
Miles Palacio <oicalap01@gmail.com>
2018-10-20 19:49:18 +0000

    cool :ice:

--- app/controllers/todo_controller.rb
+++ app/controllers/todo_controller.rb
@@ -1,5 +1,18 @@
 class TodoController < ApplicationController
- def index
-     
- end
+  def index
+  end
+  def show
+    if params[:id]=='1'
+      @todo_description="Buy Turbo" 
+    elsif params[:id] == '2'
+      @todo_description ="get bbs rims"
+    elsif params[:id] == '3'
+      @todo_description ="Buy RB26"
+    elsif params [:id] == '4'
+     @todo_description = "Research rocketbunny bodykit"
+    elsif params [:id] == '5'
+     @todo_description = "Buy lug nuts"
+    end
+  end
+  
 end
